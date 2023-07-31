65ee40b0505392e5eadadb05ff9aedda3c41a97d
HazemBZ <hazembenbz@gmail.com>
2023-07-31 19:03:42 +0100

    refactor: Move code inside lambda function

--- app.py
+++ app.py
@@ -14,37 +14,7 @@ from selenium.webdriver.chrome.service import Service
 
 
 
-enable_display = True
-error = ''
 
-try:
-
-    ## ===== Chrome part =====
-    chrome_options = ChromeOptions()
-
-    service = Service(executable_path='/opt/driver/chromedriver')
-
-    if not enable_display:
-        chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--single-process")
-    chrome_options.binary_location = '/opt/chrome/chrome-linux/chrome'
-    chrome_options.add_argument("--no-zygote")
-    chrome_options.add_argument("--single-process")
-
-    print('## {DISPLAY}: ', os.environ['DISPLAY'])
-    print('#==== Starting emu display')
-    display = Display(visible=False, extra_args=[':25'], size=(800, 800))
-    display.start()
-    print('#==== Starting chrome Driver')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    print('#==== Stopping emu display')
-    display.stop()
-
-except Exception as e:
-    error = e
-    print(e)
 
 
 
@@ -55,6 +25,42 @@ def lambda_handler(event, context):
     'Lambda Handler'
     print('#===> Inside Handler')
     print(event)
+
+
+    enable_display = True
+    error = ''
+
+    try:
+
+        ## ===== Chrome part =====
+        chrome_options = ChromeOptions()
+
+        service = Service(executable_path='/opt/driver/chromedriver')
+
+        if not enable_display:
+            chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.binary_location = '/opt/chrome/chrome-linux/chrome'
+        chrome_options.add_argument("--no-zygote")
+        chrome_options.add_argument("--single-process")
+        chrome_options.add_argument("--remote-debugging-port=9222")
+
+        print('## {DISPLAY}: ', os.environ['DISPLAY'])
+        print('#==== Starting emu display')
+        print('## {DISPLAY}: ', os.environ['DISPLAY'])
+
+        print('#==== Starting chrome Driver')
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get("https://example.com/")
+        print(driver.find_element(by=By.XPATH, value="//html").text)
+        print('#==== Stopping emu display')
+
+
+    except Exception as e:
+        error = e
+        print(e)
+
 
     return {
         "statusCode": 200,
